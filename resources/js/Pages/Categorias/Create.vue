32f8969bc2ae3e327a32155fb4c41264256c72a6
Daniel <a329545@uach.mx>
2022-04-07 16:13:45 -0600

    act

---
+++ resources/js/Pages/Categorias/Create.vue
@@ -0,0 +1,77 @@
+<template>
+    <app-layout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Crear
+            </h2>
+        </template>
+
+        <div>
+            <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
+                <div class="mt-5 md:mt-0 md:col-span-2">
+                    <form @submit.prevent="enviar">
+
+                        <div class="shadow overflow-hidden sm:rounded-md">
+                            <div class="px-4 py-5 bg-white sm:p-6">
+
+                                    <label class="block font-medium text-sm text-gray-700">
+                                        Nombre de la Categoria
+                                    </label>
+
+                                    <textarea 
+                                        class="form-input w-full rounded-md shadow-sm"
+                                        v-model="form.category_name"
+                                    ></textarea>
+
+                                    <label class="block font-medium text-sm text-gray-700">
+                                        Estatus
+                                    </label>
+
+                                    <textarea 
+                                        class="form-input w-full rounded-md shadow-sm"
+                                        v-model="form.category_status"
+                                        rows="8"
+                                    ></textarea>
+                            
+                                    <button 
+                                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
+                                    >Crear</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    export default defineComponent({
+        components: {
+            AppLayout,
+        },
+        data () {
+            return {
+                form: {
+                    category_name: '',
+                    category_status: ''
+                }
+            }
+        },
+        methods: {
+            enviar() {
+                
+                this.$inertia.post(this.route('categorias.store'), this.form)
+            }
+        }
+    })
+</script>
--- resources/js/Pages/Notas/Create.vue
+++ resources/js/Pages/Notas/Create.vue
@@ -2,39 +2,50 @@
     <app-layout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Crear 
+                Crear
             </h2>
         </template>
 
-        <div class="py-12"> 
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div>
+            <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
+                <div class="mt-5 md:mt-0 md:col-span-2">
                     <form @submit.prevent="enviar">
 
-                        <label class="block font-medium text-sm text-gray-700">
-                            Título
-                        </label>
+                        <div class="shadow overflow-hidden sm:rounded-md">
+                            <div class="px-4 py-5 bg-white sm:p-6">
 
-                        <textarea 
-                            class="form-input w-full rounded-md shadow-sm"
-                            v-model="form.titulo"
-                        ></textarea>
+                                    <label class="block font-medium text-sm text-gray-700">
+                                        Título
+                                    </label>
 
-                        <label class="block font-medium text-sm text-gray-700">
-                            Contenido
-                        </label>
+                                    <textarea 
+                                        class="form-input w-full rounded-md shadow-sm"
+                                        v-model="form.titulo"
+                                    ></textarea>
 
-                        <textarea 
-                            class="form-input w-full rounded-md shadow-sm"
-                            v-model="form.contenido"
-                            rows="8"
-                        ></textarea>
+                                    <label class="block font-medium text-sm text-gray-700">
+                                        Contenido
+                                    </label>
 
-                        <button 
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
-                        >Crear</button>
-                        
+                                    <textarea 
+                                        class="form-input w-full rounded-md shadow-sm"
+                                        v-model="form.contenido"
+                                        rows="8"
+                                    ></textarea>
+                            
+                                    <button 
+                                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
+                                    >Crear</button>
+                            </div>
+                        </div>
                     </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 </div>
             </div>
         </div>
@@ -44,13 +55,12 @@
 <script>
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
-
     export default defineComponent({
         components: {
             AppLayout,
         },
-        data() {
-            return{
+        data () {
+            return {
                 form: {
                     titulo: '',
                     contenido: ''
@@ -58,7 +68,8 @@
             }
         },
         methods: {
-            enviar(){
+            enviar() {
+                
                 this.$inertia.post(this.route('noticias.store'), this.form)
             }
         }
